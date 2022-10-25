cae4323125af8896489c2b17ca0520be1bca47d6
omboisaac <omboisaac@gmail.com>
2022-10-25 14:56:36 -0300

    Oct 25 after class

--- Visualization.R
+++ Visualization.R
@@ -1,7 +1,7 @@
 library(ggplot2)
-<<<<<<< HEAD
+
 library(tidyverse)
-=======
+
 
 #How the marital status of the person impacts the chances of Stroke?
 ggplot(data = Sample_Data, mapping = aes(x=ever_married))
@@ -14,28 +14,30 @@ ggplot(data = Sample_Data, mapping = aes(x=ever_married))
 #As many people say, various preventative steps such as switching to a healthy lifestyle by having a heart healthy diet, aiming for healthy weight, proper stress management, quitting smoking will help us to reduce the risk of having a stroke. Is this statement correct?
   
 
->>>>>>> 137ed57466a791306e71d11bba737d6236d7d291
+
 ggplot(data = Sampled, mapping = aes(x=ever_married, fill=stroke)) + geom_bar()
 
 ggplot(data = Sampled, mapping = aes(x=heart_disease, fill=stroke)) + geom_bar()
 
-<<<<<<< HEAD
+
 #histogram showing distribution of age corresponding to stroke or not
-ggplot(data=newdata, mapping= aes(x=age, fill=stroke)) +
-  geom_histogram(binwidth = 15, color="black")
+ggplot(data=StrokeData, mapping= aes(x=age, fill=stroke)) +
+  geom_histogram(binwidth = 5, color="black")
+
+#boxplot of 
+ggplot(data=StrokeData, mapping = aes(x=gender, y=age, group=gender))+ 
+  geom_boxplot(aes(fill=gender))
+
 
 #facet of geom point -> age vs bmi showing stroke or not
 ggplot(data = newdata, mapping = aes(x=age, y=bmi))+geom_point(aes(color=stroke))+
   facet_grid(~stroke)
 
 #facet of geom point -> glucose lvl vs bmi showing stroke or not
-ggplot(data = newdata, mapping = aes(x=smoking_status))+geom_bar(aes(color=stroke))
+ggplot(data = newdata, mapping = aes(x=smoking_status,fill=stroke))+geom_bar(position = "fill")
 
 
 #facet of geom point -> age vs bmi showing stroke or not
 ggplot(data = StrokeData, mapping = aes(x=age, y=bmi))+geom_point(aes(color=work_type))
   
-
-=======
->>>>>>> 137ed57466a791306e71d11bba737d6236d7d291
 
